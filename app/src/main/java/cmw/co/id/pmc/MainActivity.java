6aee16706f8da15fac5397e2f5f5206896bf8014
abdulghanni <abdulghanni2@gmail.com>
2018-01-24 17:17:52 +0700

    Initial commit

---
+++ app/src/main/java/cmw/co/id/pmc/MainActivity.java
@@ -0,0 +1,80 @@
+package cmw.co.id.pmc;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn_logout;
+    private TextView mTextMessage;
+    SharedPreferences sharedpreferences;
+    public static final String TAG_ID = "id";
+    public static final String TAG_USERNAME = "username";
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn_logout = (Button) findViewById(R.id.btn_logout);
+        TextView txt_id, txt_username;
+        String id, username;
+
+        sharedpreferences = getSharedPreferences(Login.my_shared_preferences, Context.MODE_PRIVATE);
+
+        id = getIntent().getStringExtra(TAG_ID);
+        username = getIntent().getStringExtra(TAG_USERNAME);
+
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                // update login session ke FALSE dan mengosongkan nilai id dan username
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+                editor.putBoolean(Login.session_status, false);
+                editor.putString(TAG_ID, null);
+                editor.putString(TAG_USERNAME, null);
+                editor.commit();
+
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                finish();
+                startActivity(intent);
+            }
+        });
+
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
---
+++ app/src/main/java/cmw/co/id/pmc/Server.java
@@ -0,0 +1,9 @@
+package cmw.co.id.pmc;
+
+/**
+ * Created by CMW on 24/01/2018.
+ */
+
+public class Server {
+    public static final String URL = "http://portal.cmw.co.id/pmc/";
+}
